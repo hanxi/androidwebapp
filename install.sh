662dcbbaba42bba9f871457a9e9aac79d8ced403
hanxi <hanxi.info@gmail.com>
2014-07-24 11:07:13 +0800

    rm

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-cp -rf /Users/hrj/tools/pyfm/gst-python-1.2.0/tmp/inverter/* assets/
-ant debug
-adb uninstall com.hanxi.inverter && adb install bin/inverter-debug.apk
+cp -rf /home/hanxi/Documents/mygame/InverterChinese/* assets/
+ant debug && adb uninstall com.hanxi.inverter && adb install bin/inverter-debug.apk
